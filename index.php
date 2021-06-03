19ea069af0e7416e64f2003f74bbb0e07f0080b1
Felipe Silveira <lipsmega@gmail.com>
2021-06-03 20:37:43 -0300

    adição da index.php com autoloader

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//autoload
+spl_autoload_register( function($class) {
+    if (file_exists($class . '.php')) {
+        require_once $class . '.php';
+    }
+});
+
+$class = isset($_REQUEST['class']) ? $_REQUEST['class'] : 'CursoList';
+$method = isset($_REQUEST['method']) ? $_REQUEST['method'] : null;
+
+if (class_exists($class))
+{
+    $page = new $class( $_REQUEST );
+    
+    if (!empty($method) AND method_exists($class, $method))
+    {
+        $page->$method( $_REQUEST );
+    }
+    $page->show();
+}
